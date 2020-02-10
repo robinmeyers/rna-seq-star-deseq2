bade3528351357759bd58d3ba783aa627330ed29
robinmeyers <robin.m.meyers@gmail.com>
2020-02-10 09:47:37 -0800

    arrange rows of coldata

--- scripts/deseq2-init.R
+++ scripts/deseq2-init.R
@@ -15,7 +15,7 @@ if (snakemake@threads > 1) {
 # colData and countData must have the same sample order, but this is ensured
 # by the way we create the count matrix
 cts <- read.table(snakemake@input[["counts"]], header=TRUE, row.names="gene", check.names=FALSE)
-coldata <- read.table(snakemake@params[["samples"]], header=TRUE, row.names="sample", check.names=FALSE)
+coldata <- read.table(snakemake@params[["samples"]], header=TRUE, row.names="sample", check.names=FALSE)[colnames(cts),]
 
 dds <- DESeqDataSetFromMatrix(countData=cts,
                               colData=coldata,
